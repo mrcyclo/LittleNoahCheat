92ba458b596297bb95a2313ed36db97154c1e70d
Le Tuan Anh <vipboysanhdieu@gmail.com>
2022-08-04 00:54:47 +0700

    Add aobscan

--- ImGui DirectX 11 Kiero Hook/includes.h
+++ ImGui DirectX 11 Kiero Hook/includes.h
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <d3d11.h>
 #include <dxgi.h>
+#include <psapi.h>
 #include "kiero/kiero.h"
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_win32.h"
@@ -10,3 +11,56 @@
 typedef HRESULT(__stdcall* Present) (IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags);
 typedef LRESULT(CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
 typedef uintptr_t PTR;
+
+char* ScanBasic(const char* pattern, const char* mask, char* begin, intptr_t size)
+{
+	intptr_t patternLen = strlen(mask);
+
+	//for (int i = 0; i < size - patternLen; i++)
+	for (int i = 0; i < size; i++)
+	{
+		bool found = true;
+		for (int j = 0; j < patternLen; j++)
+		{
+			if (mask[j] != '?' && pattern[j] != *(char*)((intptr_t)begin + i + j))
+			{
+				found = false;
+				break;
+			}
+		}
+		if (found)
+		{
+			return (begin + i);
+		}
+	}
+	return nullptr;
+}
+
+char* ScanPattern(HMODULE hModule, const char* pattern, const char* mask)
+{
+	char* match{ nullptr };
+	DWORD oldprotect = 0;
+	MEMORY_BASIC_INFORMATION mbi{};
+
+	MODULEINFO modinfo = { 0 };
+	GetModuleInformation(GetCurrentProcess(), hModule, &modinfo, sizeof(modinfo));
+
+	char* begin = (char*)hModule;
+	for (char* curr = begin; curr < begin + modinfo.SizeOfImage; curr += mbi.RegionSize)
+	{
+		if (!VirtualQuery(curr, &mbi, sizeof(mbi)) || mbi.State != MEM_COMMIT || mbi.Protect == PAGE_NOACCESS) continue;
+
+		if (VirtualProtect(mbi.BaseAddress, mbi.RegionSize, PAGE_EXECUTE_READWRITE, &oldprotect))
+		{
+			match = ScanBasic(pattern, mask, curr, mbi.RegionSize);
+
+			VirtualProtect(mbi.BaseAddress, mbi.RegionSize, oldprotect, &oldprotect);
+
+			if (match != nullptr)
+			{
+				break;
+			}
+		}
+	}
+	return match;
+}
--- ImGui DirectX 11 Kiero Hook/main.cpp
+++ ImGui DirectX 11 Kiero Hook/main.cpp
@@ -1,4 +1,5 @@
 #include "includes.h"
+#include <iostream>
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 Present oPresent;
@@ -8,11 +9,15 @@ ID3D11Device* pDevice = NULL;
 ID3D11DeviceContext* pContext = NULL;
 ID3D11RenderTargetView* mainRenderTargetView;
 
+bool isRunning = true;
+bool isShowMenu = true;
+
 void InitImGui()
 {
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
 	io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
+	io.MouseDrawCursor = true;
 	ImGui_ImplWin32_Init(window);
 	ImGui_ImplDX11_Init(pDevice, pContext);
 }
@@ -30,14 +35,14 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 {
 	if (!init)
 	{
-		if (SUCCEEDED(pSwapChain->GetDevice(__uuidof(ID3D11Device), (void**)& pDevice)))
+		if (SUCCEEDED(pSwapChain->GetDevice(__uuidof(ID3D11Device), (void**)&pDevice)))
 		{
 			pDevice->GetImmediateContext(&pContext);
 			DXGI_SWAP_CHAIN_DESC sd;
 			pSwapChain->GetDesc(&sd);
 			window = sd.OutputWindow;
 			ID3D11Texture2D* pBackBuffer;
-			pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)& pBackBuffer);
+			pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
 			pDevice->CreateRenderTargetView(pBackBuffer, NULL, &mainRenderTargetView);
 			pBackBuffer->Release();
 			oWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
@@ -53,14 +58,44 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 	ImGui_ImplWin32_NewFrame();
 	ImGui::NewFrame();
 
-	ImGui::Begin("ImGui Window");
-	ImGui::End();
+	if (isShowMenu) {
+		ImGui::Begin("ImGui Window");
+		ImGui::End();
+	}
 
 	ImGui::Render();
 
 	pContext->OMSetRenderTargets(1, &mainRenderTargetView, NULL);
 	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 	return oPresent(pSwapChain, SyncInterval, Flags);
+}
+
+DWORD WINAPI HotKeyThread(LPVOID lpReserved)
+{
+	while (isRunning)
+	{
+		Sleep(10);
+
+		if (GetAsyncKeyState(VK_END) & 1) {
+			isRunning = false;
+		}
+
+		if (GetAsyncKeyState(VK_HOME) & 1) {
+			isShowMenu = !isShowMenu;
+		}
+	}
+
+	kiero::shutdown();
+
+	return TRUE;
+}
+
+DWORD WINAPI InitCheatThread(LPVOID lpReserved)
+{
+	HMODULE hModule = GetModuleHandle("GameAssembly.dll");
+	uintptr_t address = (uintptr_t)ScanPattern(hModule, "\x48\x8B\x4B\x18\x48\x85\xC9\x0F\x84\x81\x01\x00\x00\x45", "xxxxxxxxxxxxxx");
+
+	return TRUE;
 }
 
 DWORD WINAPI MainThread(LPVOID lpReserved)
@@ -70,7 +105,7 @@ DWORD WINAPI MainThread(LPVOID lpReserved)
 	{
 		if (kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success)
 		{
-			kiero::bind(8, (void**)& oPresent, hkPresent);
+			kiero::bind(8, (void**)&oPresent, hkPresent);
 			init_hook = true;
 		}
 	} while (!init_hook);
@@ -84,6 +119,8 @@ BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
 	case DLL_PROCESS_ATTACH:
 		DisableThreadLibraryCalls(hMod);
 		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
+		CreateThread(nullptr, 0, HotKeyThread, hMod, 0, nullptr);
+		CreateThread(nullptr, 0, InitCheatThread, hMod, 0, nullptr);
 		break;
 	case DLL_PROCESS_DETACH:
 		kiero::shutdown();
